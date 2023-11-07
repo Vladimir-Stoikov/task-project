a2b592d095e5acef107758a3afb73b6f77558f49
Vladimir Stoikov <vspsmoke@gmail.com>
2023-11-07 21:09:50 +0300

    base setup routes and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 
-export function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={} />
-      
-    </Routes>
-  </BrowserRouter>;
+import ToDoList from 'components/Pages/ToDoList/ToDoList';
+import AddToDo from 'components/Pages/AddToDo/AddToDo';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/addtodo">Add ToDo</Link>
+          </li>
+          <li>
+            <Link to="/">All To Dos</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<ToDoList />} />
+        <Route path="/addtodo" element={<AddToDo />} />
+        <Route path="/" element={<ToDoList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
